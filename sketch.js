16617235d82ad54b1731e7fdbd7b912d248bb6fd
KhyatiBisani12 <80102728+KhyatiBisani12@users.noreply.github.com>
2021-06-06 10:42:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,33 @@
+var hero;
+var heroImg;
+var thanos;
+var thanosImg;
+var obstacles;
+var invisibleRope;
 
 
 function preload() {
 //preload the images here
-
+  heroImg = loadImage("images/ironman.png");
+  thanosImg = loadImage("images/thanos.jpg");
 }
 
 function setup() {
   createCanvas(3000, 800);
   // create sprites here
-
+  hero = createSprite(500,700,20,20);
+  hero.addImage(heroImg);
+  
+  thanos = createSprite(700,700,20,20);
+  thanos.addImage(thanosImg);
+  
 }
 
 function draw() {
   background(0);
 
-}
 
+
+  
+}
+drawSprites();
